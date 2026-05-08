34f928b7fca9ca49fe52c87f9b03a2484293d8cc
Anderson Marques <andersonmarques.ci@gmail.com>
2026-05-07 23:43:34 -0300

    Alter produce to generate invalid events for dlq test

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -35,37 +35,68 @@ func main() {
 	}
 
 	for {
-		eventType := eventTypes[rand.Intn(len(eventTypes))]
 
-		ev := event.Event{
-			ID:        uuid.NewString(),
-			TenantID:  "visa",
-			EventType: eventType,
-			Timestamp: time.Now(),
-			Payload: map[string]interface{}{
-				"random": rand.Intn(1000),
-			},
+		isInvalid := rand.Intn(100) < 10
+
+		var body []byte
+		var routingKey string
+
+		if isInvalid {
+			if rand.Intn(2) == 0 {
+				body = []byte(`{"id":`)
+				routingKey = "malformed.json"
+
+				log.Printf("malformed event published")
+			} else {
+				ev := event.Event{
+					ID:        uuid.NewString(),
+					TenantID:  "visa",
+					EventType: "demo.fail_processing",
+					Timestamp: time.Now(),
+					Payload: map[string]interface{}{
+						"reason": "simulated failure",
+					},
+				}
+
+				body, _ = json.Marshal(ev)
+				routingKey = ev.EventType
+				log.Printf(
+					"poison event published | type=%s | id=%s",
+					ev.EventType,
+					ev.ID,
+				)
+			}
+		} else {
+			eventType := eventTypes[rand.Intn(len(eventTypes))]
+
+			ev := event.Event{
+				ID:        uuid.NewString(),
+				TenantID:  "visa",
+				EventType: eventType,
+				Timestamp: time.Now(),
+				Payload: map[string]interface{}{
+					"random": rand.Intn(1000),
+				},
+			}
+
+			body, _ = json.Marshal(ev)
+			routingKey = ev.EventType
+			log.Printf(
+				"valid event published | type=%s | id=%s",
+				ev.EventType,
+				ev.ID,
+			)
 		}
 
-		body, err := json.Marshal(ev)
-		if err != nil {
-			log.Printf("marshal error: %v", err)
-			continue
-		}
+		err = producers.EventProducer.Publish(
+			routingKey,
+			body,
+		)
 
-		err = producers.EventProducer.Publish(ev.EventType, body)
 		if err != nil {
 			log.Printf("publish error: %v", err)
-			continue
 		}
-
-		log.Printf(
-			"event published | type=%s | id=%s",
-			ev.EventType,
-			ev.ID,
-		)
 
 		time.Sleep(2 * time.Second)
 	}
-
 }
