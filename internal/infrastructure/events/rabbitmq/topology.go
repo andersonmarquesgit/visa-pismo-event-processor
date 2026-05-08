6b65884b1eb52e6d7dfc5205326f6c1b6921653f
Anderson Marques <andersonmarques.ci@gmail.com>
2026-05-08 17:47:33 -0300

    Removed func with same goal that create event queue

--- internal/infrastructure/events/rabbitmq/topology.go
+++ internal/infrastructure/events/rabbitmq/topology.go
@@ -26,17 +26,6 @@ func DeclareExchange(ch *amqp.Channel, exchangeName, exchangeType string) error 
 	)
 }
 
-func DeclareQueue(ch *amqp.Channel, queueName string) (amqp.Queue, error) {
-	return ch.QueueDeclare(
-		queueName,
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-}
-
 func DeclareDeadLetterQueue(ch *amqp.Channel) (amqp.Queue, error) {
 	return ch.QueueDeclare(
 		EventsDeadLetterQueue,
